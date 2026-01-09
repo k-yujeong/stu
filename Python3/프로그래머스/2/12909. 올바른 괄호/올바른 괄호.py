90e1e2504d77209740ae8a9e534f599e25211d14
k-yujeong <rkddbwjd127@gmail.com>
2026-01-09 09:07:04 +0900

    [level 2] Title: 올바른 괄호, Time: 6.67 ms, Memory: 9.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ Python3/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,16 +1,7 @@
-# 올바른 괄호
 def solution(s):
-    stack = []
-    for ch in s:
-        if ch == '(':
-            stack.append(ch)
-        else:  # ')'
-            if not stack:
-                return False
-            stack.pop()
+    answer = True
+    
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
 
-    return not stack
-
-    
-    
-    
+    return True
