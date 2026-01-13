caed9a99ef7b9448c00035352cad09287e2e80b9
k-yujeong <rkddbwjd127@gmail.com>
2026-01-13 17:42:52 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 36944 KB -BaekjoonHub

--- Python/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ Python/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,16 +1,7 @@
-# 알파벳 찾기
 import string
 lowercase_letters = list(string.ascii_lowercase)
-lowercase_letters
-
-result = []
 
 S = input()
 
-for letter in lowercase_letters:
-    if letter in S:
-        result.append(S.index(letter))
-    else:
-        result.append(-1)
-
-print(*result)
+for s in lowercase_letters:
+    print(S.find(s), end =' ')
