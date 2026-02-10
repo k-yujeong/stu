414de06908e9132a50c40d68369a23dff9514ec2
k-yujeong <rkddbwjd127@gmail.com>
2026-02-10 17:13:33 +0900

    큰 수 만들기.py

--- Python3/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ Python3/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -4,10 +4,9 @@ def solution(number, k):
         while stack and k>0 and stack[-1] < digit:  # stack == True, 
             stack.pop()
             k-=1    # 제거 숫자 낮춤
-        stack.append(digit)
+        stack.append(digit)    # 큰 수 삽입
     if k>0:
-        stack = stack[:-k]
-    
+        stack = stack[:-k]    # 더 이상 키울 수 없으므로 끝에 슬라이싱
             
             
     return "".join(stack)
